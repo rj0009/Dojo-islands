6d6729899509df09b0e40e36e488647f97de71cf
rj0009 <33339054+rj0009@users.noreply.github.com>
2026-04-13 20:56:59 +0800

    Add DojoIslands React component

---
+++ src/dojo-islands.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DojoIslands = () => {
+    return (
+        <div>
+            <h1>Welcome to the Dojo Islands</h1>
+            <p>Explore the beauty of the islands!</p>
+        </div>
+    );
+};
+
+export default DojoIslands;
